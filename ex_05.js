96afed9ea7bd1dd00ac14fa9b26bee1e21ea4633
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-03 12:31:03 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var addToCartButton = document.getElementById("addToCart");
+
+    
+    addToCartButton.addEventListener("click", function() {
+        
+        alert("Thanks!");
+    });
+});
